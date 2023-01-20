78266f1f85442ef54f6254c5bea15a07beaac5be
Stanislav Sumin <stanislav.sumin@myoffice.team>
2023-01-20 16:55:14 +0300

    add comm

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import randint
 
 
 def attack(char_name, char_class):
+    """Описывает работу функции расчёта урона."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -14,6 +15,7 @@ def attack(char_name, char_class):
 
 
 def defence(char_name, char_class):
+    """Описывает работу функции расчёта защиты."""
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -23,6 +25,7 @@ def defence(char_name, char_class):
 
 
 def special(char_name, char_class):
+    """Описывает работу функции расчёта умения."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -33,6 +36,7 @@ def special(char_name, char_class):
 
 
 def start_training(char_name, char_class):
+    """Стартовая функция."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -57,6 +61,7 @@ def start_training(char_name, char_class):
 
 
 def choice_char_class():
+    """Стартовая функция."""
     approve_choice = None
     char_class = None
     while approve_choice != 'y':
@@ -78,7 +83,7 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == "__main__":
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name = input('...назови себя: ')
